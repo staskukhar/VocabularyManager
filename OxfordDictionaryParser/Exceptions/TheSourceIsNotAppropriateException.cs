3006c4e384f6cc85d3ca66ada594aa2626a485b5
staskukhar <staskuk2005@gmail.com>
2024-01-13 17:44:05 +0200

    adds exception for the pasring service

---
+++ OxfordDictionaryParser/Exceptions/TheSourceIsNotAppropriateException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxfordDictionaryParser.Exceptions
+{
+    public class TheSourceIsNotAppropriateException : Exception
+    {
+        public TheSourceIsNotAppropriateException() { }
+        public TheSourceIsNotAppropriateException(string message) 
+            : base(message) { }
+        public TheSourceIsNotAppropriateException(string message, Exception inner) 
+            : base(message, inner) { }
+    }
+}
