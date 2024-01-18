3666c07f792940ad502633c0adf7d367ac63e807
staskukhar <staskuk2005@gmail.com>
2024-01-18 14:34:50 +0200

    adds interface for image parser

---
+++ DictionaryParser/Services/IImageParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryParserLib.Services
+{
+    public interface IImageParser<T>
+    {
+        public Task<T> GetImageLinksByWord(string url, string word, bool colorfulFilter = true);
+    }
+}
