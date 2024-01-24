de38962a45ea23a4479e26ce93215879a6122536
staskukhar <staskuk2005@gmail.com>
2024-01-24 16:09:33 +0200

    adds controller with post method

---
+++ DictionaryManagerAPI/Controllers/WordListController.cs
@@ -0,0 +1,36 @@
+﻿using DictionaryParser.Exceptions;
+using DictionaryParser.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DictionaryManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordListController : ControllerBase
+    {
+        [HttpPost("getwordlist")]
+        public async Task<IActionResult> GetWordListByUrlAsync([FromBody] string url)
+        {
+            var dictionaryParser = new OxfordDictionaryParser();
+            try
+            {
+                IEnumerable<ParsingWordDTO> wordList = await dictionaryParser.GetWordListByLinkAsync(url);
+
+                return Ok(wordList);
+            }
+            catch (ArgumentNullException e) 
+            {
+                return BadRequest(e.Message);
+            }
+            catch(NullReferenceException e) 
+            { 
+                return BadRequest(e.Message);
+            }
+            catch(TheSourceIsNotAppropriateException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
