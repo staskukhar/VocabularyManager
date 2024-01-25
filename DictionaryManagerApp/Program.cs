5ed193412629ec789a668a50a0e4f4ee7705bfec
staskukhar <staskuk2005@gmail.com>
2024-01-25 16:05:10 +0200

    adds configuration section and sets http client DI

--- DictionaryManagerApp/Program.cs
+++ DictionaryManagerApp/Program.cs
@@ -1,11 +1,18 @@
 using DictionaryManagerApp;
+using DictionaryManagerApp.Models.Configurations;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Dynamic;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.Configuration.GetSection("URIStrings")["DictionaryManagerApi"])
+});
 
 await builder.Build().RunAsync();
