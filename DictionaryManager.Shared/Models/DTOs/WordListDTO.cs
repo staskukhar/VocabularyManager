abb5525a3d942446fb7db94e3817dc1d8e08fec4
staskukhar <staskuk2005@gmail.com>
2024-04-15 15:33:59 +0300

    applies changes for DTOs

---
+++ DictionaryManager.Shared/Models/DTOs/WordListDTO.cs
@@ -0,0 +1,18 @@
+﻿using DictionaryManager.Shared.Models.Data;
+
+namespace DictionaryManager.Shared.Models.DTOs
+{
+    public class WordListDTO
+    {
+        public int? Id { get; init; }
+        public string ListName { get; set; }
+        public string? SourceUrl { get; set; }
+        public List<WordDTO>? Words { get; set; }
+        public WordListDTO(string listName, string? sourceUrl) 
+        {
+            ListName = listName;
+            SourceUrl = sourceUrl;
+        }
+        public WordListDTO() { }
+    }
+}
