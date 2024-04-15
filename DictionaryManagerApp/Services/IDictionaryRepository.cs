17d41d924aa750395e378a2f6e3e63e38b4c4fc5
staskukhar <staskuk2005@gmail.com>
2024-04-15 15:41:54 +0300

    implements dictionary repository

---
+++ DictionaryManagerApp/Services/IDictionaryRepository.cs
@@ -0,0 +1,18 @@
+﻿using DictionaryManager.Shared.Models.DTOs;
+
+namespace DictionaryManagerApp.Services
+{
+    public interface IDictionaryRepository<WL, W>
+    {
+        WL Dictionary { get; }
+        bool HasChanges { get; }
+        void SetDictionary(WL dictionary);
+        void RemoveWordAtIndex(int index);
+        void UpdateWordAtIndex(W word, int index);
+        void AddWord(W word);
+        void SubscribeOnChangeAction(Action action);
+        void UnsubscribeOnChangeAction(Action action);
+        void ChangesApplied();
+        public void NotifyStateHasChanged();
+    }
+}
