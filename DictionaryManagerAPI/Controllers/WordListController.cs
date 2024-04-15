3effdee81aa2ce56803a55e4f0bf34046bad050a
staskukhar <staskuk2005@gmail.com>
2024-04-15 15:37:14 +0300

    adds endpoints for word lists management

--- DictionaryManagerAPI/Controllers/WordListController.cs
+++ DictionaryManagerAPI/Controllers/WordListController.cs
@@ -1,7 +1,11 @@
-﻿using DictionaryParser.Exceptions;
+﻿using DictionaryManager.Shared.Models.Data;
+using DictionaryManager.Shared.Models.DTOs;
+using DictionaryManagerAPI.Services.Repositories;
+using DictionaryParser.Exceptions;
 using DictionaryParser.Services;
-using Microsoft.AspNetCore.Http;
+using DictionaryParserLib.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DictionaryManagerAPI.Controllers
 {
@@ -9,27 +13,88 @@ namespace DictionaryManagerAPI.Controllers
     [ApiController]
     public class WordListController : ControllerBase
     {
+        private readonly IWordListRepository _wordListRepository;
+        public WordListController(IWordListRepository wordListRepository)
+        {
+            _wordListRepository = wordListRepository;
+        }
+
         [HttpPost("getwordlist")]
         public async Task<IActionResult> GetWordListByUrlAsync([FromBody] string url)
         {
             var dictionaryParser = new OxfordDictionaryParser();
             try
             {
-                IEnumerable<ParsingWordDTO> wordList = await dictionaryParser.GetWordListByLinkAsync(url);
+                IEnumerable<WordDTO> wordList = await dictionaryParser
+                    .GetWordListByLinkAsync(url, OxfordDictionaryValidator.IsWordObjectDataValid);
 
                 return Ok(wordList);
             }
-            catch (ArgumentNullException e) 
+            catch(Exception ex) when(
+                ex is ArgumentNullException || 
+                ex is NullReferenceException || 
+                ex is TheSourceIsNotAppropriateException
+            )
             {
-                return BadRequest(e.Message);
+                return BadRequest(ex.Message);
             }
-            catch(NullReferenceException e) 
-            { 
-                return BadRequest(e.Message);
-            }
-            catch(TheSourceIsNotAppropriateException e)
+        }
+        [HttpPost("createwordlist")]
+        public IActionResult CreateWordList(
+            [FromBody] WordListDTO wordList)
+        {
+            WordList wlResult = _wordListRepository.Add(
+                new WordList(wordList.ListName, wordList.SourceUrl));
+            try
             {
-                return BadRequest(e.Message);
+                _wordListRepository.Save();
+                return Ok(wlResult.Id);
+            }
+            catch (Exception ex) when (
+                ex is DbUpdateException || ex is DbUpdateConcurrencyException)
+            {
+                return BadRequest("Sorry, the create operation failed!");
+            }
+        }
+        [HttpGet("getdictionaries")]
+        public IActionResult GetDictionaries()
+        {
+            return Ok(_wordListRepository.GetAll());
+        }
+        [HttpGet("getwordlistbyid")]
+        public IActionResult GetWordListWithWordsByDictionaryId(
+            [FromQuery] int dictionaryId
+        )
+        {
+            try
+            {
+                WordList wordList = _wordListRepository.GetWithRelationsById(dictionaryId);
+
+                return Ok(wordList);
+            }
+            catch(ArgumentNullException) 
+            {
+                return BadRequest("Id parameter is required and should be defined.");
+            }
+            catch(InvalidOperationException)
+            {
+                return BadRequest($"There isn't any list with id: {dictionaryId}");
+            }
+        }
+        [HttpPut("updatewordlist")]
+        public IActionResult UpdateDictionaryWithRelations(
+            [FromBody] WordList wordList)
+        {
+            WordList updatedWordList = _wordListRepository.Update(wordList);
+            try
+            {
+                _wordListRepository.Save();
+                return Ok(updatedWordList.Id);
+            }
+            catch (Exception ex) when (
+                ex is DbUpdateException || ex is DbUpdateConcurrencyException)
+            {
+                return BadRequest("Sorry, the update operation failed!");
             }
         }
     }
