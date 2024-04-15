8a882d2ee2464aa18cbb6c5ccf69c2983b8cd5f2
staskukhar <staskuk2005@gmail.com>
2024-04-15 15:42:38 +0300

    adds word cloner service

---
+++ DictionaryManagerApp/Services/WordCloner.cs
@@ -0,0 +1,21 @@
+﻿using DictionaryManager.Shared.Models.DTOs;
+
+namespace DictionaryManagerApp.Services
+{
+    public class WordCloner
+    {
+        public static WordDTO CloneWord(WordDTO word)
+        {
+            WordDTO clone = new WordDTO()
+            {
+                Id = word.Id,
+                WordContent = word.WordContent,
+                Lexeme = word.Lexeme,
+                LevelAttribute = word.LevelAttribute,
+                Defenition = word.Defenition,
+                WordListId = word.WordListId,
+            };
+            return clone;
+        }
+    }
+}
