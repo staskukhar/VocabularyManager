a60f2e69b854358f1090a3c7668c842729d2ec3e
staskukhar <staskuk2005@gmail.com>
2024-05-06 15:02:47 +0300

    adds bind model for http options

---
+++ src/VocabularyManager.BlazorApp/Models/Configurations/HttpClientOptions.cs
@@ -0,0 +1,42 @@
+﻿namespace VocabularyManager.BlazorApp.Models.Configurations
+{
+    public class HttpClientOptions
+    {
+        public const string SectionKey = "HttpClient";
+        public string ApiBaseURL { get; set; } = String.Empty;
+        public WordPathes WordTree { get; set; }
+        public WordListPathes WordListTree { get; set; }
+        public HttpClientOptions()
+        {
+
+        }
+
+    }
+    public class WordPathes
+    {
+        public const string SectionKey = "Word";
+        public string PathPrefix { get; set; } = String.Empty;
+        public string Add { get; set; } = String.Empty;
+        public string AddWords { get; set; } = String.Empty;
+        public string Delete { get; set; } = String.Empty;
+        public string Update { get; set; } = String.Empty;
+        public WordPathes()
+        {
+            
+        }
+    }
+    public class WordListPathes
+    {
+        public const string SectionKey = "WordList";
+        public string PathPrefix { get; set; } = String.Empty;
+        public string GetWordList { get; set; } = String.Empty;
+        public string Create { get; set; } = String.Empty;
+        public string GetWordLists { get; set; } = String.Empty;
+        public string GetById { get; set; } = String.Empty;
+        public string Update { get; set; } = String.Empty;
+        public WordListPathes()
+        {
+            
+        }
+    }
+}
