d58d00bddf06b118d2a69db68a3318507a5a2082
staskukhar <staskuk2005@gmail.com>
2024-05-06 14:57:15 +0300

    adds validators for dtos

---
+++ src/VocabularyManager.UseCases/Validators/WordDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using VocabularyManager.UseCases.DTOs;
+
+namespace VocabularyManager.UseCases.Validators
+{
+    public class WordDTOValidator : AbstractValidator<WordDTO>
+    {
+        public WordDTOValidator()
+        {
+            RuleFor(word => word.WordContent)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .WithMessage("Word content is required.")
+                .NotEmpty();
+        }
+    }
+}
---
+++ src/VocabularyManager.UseCases/Validators/WordListDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using VocabularyManager.UseCases.DTOs;
+
+namespace VocabularyManager.UseCases.Validators
+{
+    public class WordListDTOValidator : AbstractValidator<WordListDTO>
+    {
+        public WordListDTOValidator() 
+        {
+            RuleFor(wl => wl.ListName)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .WithMessage("List's name is required.")
+                .NotEmpty();
+        }
+    }
+}
