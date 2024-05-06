10bfcd8787569d90d1a2e48e32ba17f05410c5f9
staskukhar <staskuk2005@gmail.com>
2024-05-06 14:58:21 +0300

    adds http path builder service

---
+++ src/VocabularyManager.BlazorApp/Services/HttpPathBuilder.cs
@@ -0,0 +1,90 @@
+﻿using VocabularyManager.BlazorApp.Models.Configurations;
+
+namespace VocabularyManager.BlazorApp.Services
+{
+    public class HttpPathBuilder
+    {
+        private HttpClientOptions _httpOptions;
+        public HttpPathBuilder(HttpClientOptions httpOptions) 
+        {
+            _httpOptions = httpOptions;
+        }
+
+        public string AddWordEndpoint(int wordListId)
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL, 
+                _httpOptions.WordTree.PathPrefix,
+                _httpOptions.WordTree.Add,
+                $"?wordListId={wordListId}"
+            );
+        }
+        public string AddWordsEndpoint(int wordListId)
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordTree.PathPrefix,
+                _httpOptions.WordTree.AddWords,
+                $"?wordlistid={wordListId}"
+            );
+        }
+        public string DeleteWordEndpoint(int wordId)
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordTree.PathPrefix,
+                _httpOptions.WordTree.Delete,
+                $"?id={wordId}"
+            );
+        }
+        public string ParseListOfWordEndpoint()
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordListTree.PathPrefix,
+                _httpOptions.WordListTree.GetWordList
+            );
+        }
+        public string CreateWordListEndpoint()
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordListTree.PathPrefix,
+                _httpOptions.WordListTree.Create
+            );
+        }
+        public string GetWordListsEndpoint()
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordListTree.PathPrefix,
+                _httpOptions.WordListTree.GetWordLists
+            );
+        }
+        public string GetWordListByIdEndpoint(int wordListId)
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordListTree.PathPrefix,
+                _httpOptions.WordListTree.GetById,
+                $"?wordlistid={wordListId}"
+            );
+        }
+        public string UpdateWordListEndpoint()
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordListTree.PathPrefix,
+                _httpOptions.WordListTree.Update
+            );
+        }
+        public string UpdateWordEndpoint()
+        {
+            return String.Concat(
+                _httpOptions.ApiBaseURL,
+                _httpOptions.WordTree.PathPrefix,
+                _httpOptions.WordTree.Update
+            );
+        }
+    }
+}
