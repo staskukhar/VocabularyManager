6ff7c967552dc889664123936ab5dc598548c50f
staskukhar <staskuk2005@gmail.com>
2024-05-06 14:58:36 +0300

    adds http service

---
+++ src/VocabularyManager.BlazorApp/Services/HttpService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+
+namespace VocabularyManager.BlazorApp.Services
+{
+    public class HttpService
+    {
+        HttpClient _httpClient;
+        public HttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public virtual async Task<HttpResponseMessage> Get(string requestUrl)
+        {
+            return await _httpClient.GetAsync(requestUrl);
+        }
+        public virtual  async Task<HttpResponseMessage> PostWithJson<TRequest>(string requestUrl, TRequest data)
+        {
+            return await _httpClient.PostAsJsonAsync(requestUrl, data);
+        }
+        public virtual async Task<HttpResponseMessage> Delete(string requestUrl)
+        {
+            return await _httpClient.DeleteAsync(requestUrl);
+        }
+        public virtual async Task<HttpResponseMessage> Put<TRequest>(string requestUrl, TRequest data)
+        {
+            return await _httpClient.PutAsJsonAsync(requestUrl, data);
+        }
+    }
+}
