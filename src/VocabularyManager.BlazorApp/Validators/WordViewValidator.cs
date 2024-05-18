9961b3491677b64e12ed98a8f26079af91ceb234
staskukhar <staskuk2005@gmail.com>
2024-05-18 20:36:51 +0300

    adds validation for client's models

---
+++ src/VocabularyManager.BlazorApp/Validators/WordViewValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using VocabularyManager.BlazorApp.Models.Views;
+
+namespace VocabularyManager.BlazorApp.Validators
+{
+    public class WordViewValidator : AbstractValidator<WordView>
+    {
+        public WordViewValidator() 
+        {
+            RuleFor(word => word.WordContent)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .WithMessage("Word content is required.")
+                .NotEmpty();
+        }
+    }
+}
