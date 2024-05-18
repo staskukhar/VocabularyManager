f5770cc1a3eb25105beda9339f9d55ce1353bc6b
staskukhar <staskuk2005@gmail.com>
2024-05-18 20:36:05 +0300

    adds models for client layer

---
+++ src/VocabularyManager.BlazorApp/Models/Views/VocabularyView.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VocabularyManager.Core.Entities;
+
+namespace VocabularyManager.BlazorApp.Models.Views
+{
+    public class VocabularyView
+    {
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public string? SourceUrl { get; set; }
+        public List<WordView> Words { get; set; } = new List<WordView> { };
+
+        public VocabularyView(){ }
+        public VocabularyView(string name, string? sourceUrl)
+        {
+            Name = name;
+            SourceUrl = sourceUrl;
+        }
+    }
+}
---
+++ src/VocabularyManager.BlazorApp/Models/Views/WordView.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using VocabularyManager.Core.Entities;
+
+namespace VocabularyManager.BlazorApp.Models.Views
+{
+    public class WordView
+    {
+        public int Id { get; init; }
+        public string WordContent { get; set; }
+        public string? Lexeme { get; set; }
+        public string? LevelAttribute { get; set; }
+        public string? Defenition { get; set; }
+        public int VocabularyId { get; set; }
+        public WordView(){ }
+        public WordView(string wordContent, string? lexeme, string? levelAttribute, string? defenition)
+        {
+            WordContent = wordContent;
+            Lexeme = lexeme;
+            LevelAttribute = levelAttribute;
+            Defenition = defenition;
+        }
+    }
+}
