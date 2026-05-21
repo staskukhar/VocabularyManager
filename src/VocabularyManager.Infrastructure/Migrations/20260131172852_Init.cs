6c5328bf21db1007d224fdd7e5f6014cd2126cfc
Stas Kukhar <staskuk2005@gmail.com>
2026-05-21 15:53:19 +0300

    adds migrations

---
+++ src/VocabularyManager.Infrastructure/Migrations/20260131172852_Init.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VocabularyManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vocabularies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:IdentitySequenceOptions", "'1', '1', '', '', 'False', '1'")
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    SourceUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vocabularies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:IdentitySequenceOptions", "'1', '1', '', '', 'False', '1'")
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    WordContent = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    VocabularyId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Words_Vocabularies_VocabularyId",
+                        column: x => x.VocabularyId,
+                        principalTable: "Vocabularies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meanings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:IdentitySequenceOptions", "'1', '1', '', '', 'False', '1'")
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LexemeType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Definition = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    Level = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    WordId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meanings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Meanings_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meanings_WordId",
+                table: "Meanings",
+                column: "WordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Words_VocabularyId",
+                table: "Words",
+                column: "VocabularyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Words_WordContent_VocabularyId",
+                table: "Words",
+                columns: new[] { "WordContent", "VocabularyId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meanings");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+
+            migrationBuilder.DropTable(
+                name: "Vocabularies");
+        }
+    }
+}
